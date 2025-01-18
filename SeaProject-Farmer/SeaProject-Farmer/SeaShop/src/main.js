6da8423693be43a1aeac999a6c990996f0becc62
围巾 <2827348531@qq.com>
2025-01-18 10:45:47 +0900

    修改

--- SeaProject-Farmer/SeaProject-Farmer/SeaShop/src/main.js
+++ SeaProject-Farmer/SeaProject-Farmer/SeaShop/src/main.js
@@ -10,6 +10,9 @@ import BottomNavigation from './components/BottomNavigation.vue'
 
 const app = createApp(App)
 
+// 将router实例暴露到全局
+window.__VUE__ = router
+
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
@@ -19,4 +22,5 @@ app.use(ElementPlus)
 app.component('BottomNavigation',BottomNavigation)
 router.isReady().then(() => {
     app.mount('#app')
+    console.log('Vue应用已挂载')
   })
