1fceb2459c8bc5c5709404daf2b43478728bc055
郭伟进 <2827348531@qq.com>
2025-05-02 11:05:47 +0800

    又是依托

---
+++ SeaProject-FarmerNew/Farmer/src/data/product-detail.js
@@ -0,0 +1,415 @@
+import p1 from '../assets/p (11).jpg'
+import p2 from '../assets/p (2).jpg'
+import p3 from '../assets/p (3).jpg'
+import p4 from '../assets/p (4).jpg'
+import p5 from '../assets/p (5).jpg'
+import p6 from '../assets/p (6).jpg'
+import p7 from '../assets/p (7).jpg'
+import p8 from '../assets/p (8).jpg'
+import p9 from '../assets/p (9).jpg'
+import p10 from '../assets/p (10).jpg'
+import algae from '../assets/algae.png'
+
+export const productDetails = [
+    {
+        id: 1,
+        image: p1,
+        name: '大闸蟹',
+        price: '45 / 斤',
+        evaluation: '良好',
+        tags: ['可溯源', '品质保证', '实时监控'],
+        description: '螃蟹肉质鲜美，适合多种烹饪方式。',
+        reviews: [
+            {
+                reviewer: '李**',
+                purchaseQuantity: 3,
+                content: '螃蟹很新鲜，大小也合适，下次还会购买。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '赵**',
+                purchaseQuantity: 2,
+                content: '买回去做了酸菜螃蟹，味道超棒，草鱼很嫩。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '孙**',
+                purchaseQuantity: 4,
+                content: '包装很用心，螃蟹到家还是活蹦乱跳的，好评！',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '周**',
+                purchaseQuantity: 1,
+                content: '肉质紧实，刺相对较少，适合老人小孩吃。',
+                image: '',
+                unit: '斤'
+            }
+        ],
+        reviewRate: 100
+    },
+    {
+        id: 2,
+        image: p2,
+        name: '鲫鱼',
+        price: '15 / 斤',
+        evaluation: '优秀',
+        tags: ['可溯源', '品质保证', '实时监控'],
+        description: '鲫鱼营养丰富，适合炖汤。',
+        reviews: [
+            {
+                reviewer: '王**',
+                purchaseQuantity: 2,
+                content: '鱼很肥，味道不错，推荐购买。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '刘**',
+                purchaseQuantity: 3,
+                content: '用鲫鱼炖的汤，奶白色的，特别鲜美，家人都爱喝。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '陈**',
+                purchaseQuantity: 1,
+                content: '鲫鱼很新鲜，处理得也干净，回家直接下锅就行。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '杨**',
+                purchaseQuantity: 2,
+                content: '价格实惠，品质却一点不含糊，会回购。',
+                image: '',
+                unit: '斤'
+            }
+        ],
+        reviewRate: 100
+    },
+    {
+        id: 3,
+        image: p3,
+        name: '鲶鱼',
+        price: '18 / 斤',
+        evaluation: '优秀',
+        tags: ['可溯源', '品质保证', '实时监控'],
+        description: '鲶鱼肉质鲜嫩，无小刺，适合老人和小孩。',
+        reviews: [
+            {
+                reviewer: '吴**',
+                purchaseQuantity: 2,
+                content: '鲶鱼很新鲜，做出来的红烧鲶鱼味道太棒了。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '张**',
+                purchaseQuantity: 1,
+                content: '没有什么土腥味，肉质很嫩，孩子特别爱吃。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '胡**',
+                purchaseQuantity: 3,
+                content: '包装严实，运输过程中鱼没有受损，好评。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '林**',
+                purchaseQuantity: 2,
+                content: '价格合理，品质有保障，以后就认准这家了。',
+                image: '',
+                unit: '斤'
+            }
+        ],
+        reviewRate: 100
+    },
+    {
+        id: 4,
+        image: p4,
+        name: '黄鳝',
+        price: '30 / 斤',
+        evaluation: '良好',
+        tags: ['可溯源', '品质保证', '实时监控'],
+        description: '黄鳝营养丰富，具有滋补功效。',
+        reviews: [
+            {
+                reviewer: '朱**',
+                purchaseQuantity: 1,
+                content: '黄鳝很鲜活，用来做了黄鳝粥，味道鲜美。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '徐**',
+                purchaseQuantity: 2,
+                content: '处理得很干净，省去了自己处理的麻烦，好评。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '何**',
+                purchaseQuantity: 1,
+                content: '肉质很嫩，口感很好，下次还会买。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '郭**',
+                purchaseQuantity: 2,
+                content: '黄鳝的品质不错，和描述的一样。',
+                image: '',
+                unit: '斤'
+            }
+        ],
+        reviewRate: 100
+    },
+    {
+        id: 5,
+        image: p5,
+        name: '中华草龟',
+        price: '50 / 只',
+        evaluation: '优秀',
+        tags: ['可溯源', '品质保证', '实时监控'],
+        description: '中华草龟是优质的食材，营养丰富。',
+        reviews: [
+            {
+                reviewer: '马**',
+                purchaseQuantity: 1,
+                content: '草龟很健康，活力十足，煲汤味道鲜美。',
+                image: '',
+                unit: '只'
+            },
+            {
+                reviewer: '罗**',
+                purchaseQuantity: 2,
+                content: '包装很专业，龟龟到手没有任何损伤。',
+                image: '',
+                unit: '只'
+            },
+            {
+                reviewer: '梁**',
+                purchaseQuantity: 1,
+                content: '肉质很紧实，口感很好，值得购买。',
+                image: '',
+                unit: '只'
+            },
+            {
+                reviewer: '宋**',
+                purchaseQuantity: 1,
+                content: '和卖家描述的一样，以后还会再来。',
+                image: '',
+                unit: '只'
+            }
+        ],
+        reviewRate: 100
+    },
+    {
+        id: 6,
+        image: algae,
+        name: '小球藻',
+        price: '20 / 斤',
+        evaluation: '优秀',
+        tags: ['可溯源', '品质保证', '实时监控'],
+        description: '小球藻肉质细嫩，营养丰富。',
+        reviews: [
+            {
+                reviewer: '郑**',
+                purchaseQuantity: 2,
+                content: '小球藻很鲜活，用来做小球藻钻豆腐，味道超赞。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '谢**',
+                purchaseQuantity: 1,
+                content: '处理得很干净，没有泥沙味，好评。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '韩**',
+                purchaseQuantity: 2,
+                content: '小球藻大小均匀，品质不错。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '唐**',
+                purchaseQuantity: 1,
+                content: '价格实惠，会继续支持。',
+                image: '',
+                unit: '斤'
+            }
+        ],
+        reviewRate: 100
+    },
+    {
+        id: 7,
+        image: p7,
+        name: '福寿螺（食用级）',
+        price: '8 / 斤',
+        evaluation: '优秀',
+        tags: ['可溯源', '品质保证', '实时监控'],
+        description: '食用级福寿螺肉质鲜美，烹饪后口感好。',
+        reviews: [
+            {
+                reviewer: '冯**',
+                purchaseQuantity: 3,
+                content: '福寿螺很新鲜，炒着吃味道很不错。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '于**',
+                purchaseQuantity: 2,
+                content: '处理得很干净，没有异味，值得购买。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '董**',
+                purchaseQuantity: 1,
+                content: '价格便宜，量也足，很划算。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '萧**',
+                purchaseQuantity: 2,
+                content: '品质有保障，以后还会来买。',
+                image: '',
+                unit: '斤'
+            }
+        ],
+        reviewRate: 100
+    },
+    {
+        id: 8,
+        image: p8,
+        name: '淡水小龙虾',
+        price: '25 / 斤',
+        evaluation: '优秀',
+        tags: ['可溯源', '品质保证', '实时监控'],
+        description: '小龙虾个头大，肉白紧实，价格实惠。',
+        reviews: [
+            {
+                reviewer: '程**',
+                purchaseQuantity: 5,
+                content: '与描述一致，服务态度好，发货快，商品新鲜，认真负责。小龙虾个头大，肉白紧实，价格实惠...',
+                image: p8,
+                unit: '斤'
+            },
+            {
+                reviewer: '曹**',
+                purchaseQuantity: 3,
+                content: '小龙虾很干净，做出来的麻辣小龙虾太好吃了。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '袁**',
+                purchaseQuantity: 2,
+                content: '个头均匀，虾肉饱满，非常满意。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '邓**',
+                purchaseQuantity: 4,
+                content: '性价比很高，以后吃小龙虾就选这家了。',
+                image: '',
+                unit: '斤'
+            }
+        ],
+        reviewRate: 100
+    },
+    {
+        id: 9,
+        image: p9,
+        name: '白鲢鱼',
+        price: '8 / 斤',
+        evaluation: '优秀',
+        tags: ['可溯源', '品质保证', '实时监控'],
+        description: '白鲢鱼肉质鲜嫩，适合多种做法。',
+        reviews: [
+            {
+                reviewer: '许**',
+                purchaseQuantity: 3,
+                content: '白鲢鱼很新鲜，做的剁椒鱼头味道很棒。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '傅**',
+                purchaseQuantity: 2,
+                content: '价格实惠，鱼的品质也不错。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '沈**',
+                purchaseQuantity: 1,
+                content: '鱼肉很嫩，刺处理得也方便。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '曾**',
+                purchaseQuantity: 2,
+                content: '包装很好，鱼没有破损，好评。',
+                image: '',
+                unit: '斤'
+            }
+        ],
+        reviewRate: 100
+    },
+    {
+        id: 10,
+        image: p10,
+        name: '鳙鱼（胖头鱼）',
+        price: '13 / 斤',
+        evaluation: '良好',
+        tags: ['可溯源', '品质保证', '实时监控'],
+        description: '鳙鱼鱼头大，适合做鱼头汤。',
+        reviews: [
+            {
+                reviewer: '彭**',
+                purchaseQuantity: 2,
+                content: '鳙鱼很新鲜，鱼头炖汤特别鲜美。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '吕**',
+                purchaseQuantity: 1,
+                content: '鱼头很大，肉也很多，很满意。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '苏**',
+                purchaseQuantity: 3,
+                content: '鱼的品质不错，和卖家描述相符。',
+                image: '',
+                unit: '斤'
+            },
+            {
+                reviewer: '卢**',
+                purchaseQuantity: 2,
+                content: '价格合理，以后会经常购买。',
+                image: '',
+                unit: '斤'
+            }
+        ],
+        reviewRate: 100
+    }
+];
+
---
+++ SeaProject-FarmerNew/Farmer/src/stores/userStore.js
@@ -0,0 +1,89 @@
+// stores/userStore.js
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+
+export const useUserStore = defineStore('user', {
+  state: () => ({
+    isFamer: true,
+    isCustomer: false, 
+    isWholesalers:false,
+    isWholesalers2:false, //Wholesalers 现在是 直营店/加盟商端 Wholesalers2现在才是批发商端
+    cart:[]
+  }),
+  actions: {
+    switchToCustomer() {
+      this.isCustomer = true;
+      this.isFamer = false;
+      this.isWholesalers = false;
+      this.isWholesalers2 = false;
+    },
+    switchToFamer() {
+      this.isCustomer = false;
+      this.isFamer = true;
+      this.isWholesalers = false;
+      this.isWholesalers2 = false;
+    },
+    switchToWholesaler() {
+      this.isCustomer = false;
+      this.isFamer = false;
+      this.isWholesalers = true;
+      this.isWholesalers2 = false;
+    },
+    switchToWholesalers2() {
+      this.isCustomer = true;
+      this.isFamer = false;
+      this.isWholesalers = false;
+      this.isWholesalers2 = true;
+    },
+    addToCart(product) {
+      const existing = this.cart.find(item => item.id === product.id);
+      if (existing) {
+        existing.quantity++;
+      } else {
+        this.cart.push({
+          ...product,
+          quantity: 1,
+          addedAt: new Date().toISOString()
+        });
+      }
+      this.saveCart();
+    },
+    removeFromCart(productId) {
+      this.cart = this.cart.filter(item => item.id !== productId);
+      this.saveCart();
+    },
+    updateQuantity(productId, newQuantity) {
+      const item = this.cart.find(item => item.id === productId);
+      if (item) {
+        item.quantity = Math.max(1, newQuantity);
+        this.saveCart();
+      }
+    },
+    clearCart() {
+      this.cart = [];
+      this.saveCart();
+    },
+    // 持久化存储
+    saveCart() {
+      localStorage.setItem('shoppingCart', JSON.stringify(this.cart));
+    },
+    initialize() {
+      const savedCart = localStorage.getItem('shoppingCart');
+      if (savedCart) {
+        this.cart = JSON.parse(savedCart);
+      }
+    }
+
+  },
+  // getters: {
+  //   totalItems: (state) => state.cart.reduce((sum, item) => sum + item.quantity, 0),
+  //   totalPrice: (state) => state.cart.reduce((sum, item) => {
+  //     const price = parseFloat(item.price.split('/')[0].replace('￥', ''));
+  //     return sum + (price * item.quantity);
+  //   }, 0)
+  // }
+});
+
+// const userStore = useUserStore();
+// userStore.initialize();
